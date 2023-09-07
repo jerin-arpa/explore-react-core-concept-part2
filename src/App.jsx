01ee7f15c22f5def8f8a7f92adf30c9ac0bdf961
jerin-arpa <ismatjerrin3@gmail.com>
2023-09-08 03:09:44 +0600

    event handler

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,27 @@ import './App.css'
 
 function App() {
 
+  function handleClick() {
+    alert('Button Clicked');
+  }
+
+
+  const handleClick2 = () => {
+    alert('Button 2 Clicked')
+  }
+
+  const addTOFive = (num) => {
+    alert(num + 5);
+  }
+
+
   return (
     <>
       <h2>React Core Concepts Part2</h2>
-      <button>Click Me</button>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click 2</button>
+      <button onClick={() => { alert('Button 3 Clicked') }}>Click 3</button>
+      <button onClick={() => addTOFive(3)}>Click 4</button>
     </>
   )
 }
