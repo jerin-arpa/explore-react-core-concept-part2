e67396fbba87ee886f30ad17e7ee0b4a2530684e
jerin-arpa <ismatjerrin3@gmail.com>
2023-09-09 23:20:03 +0600

    use useEffect integrate state

--- src/Friends.jsx
+++ src/Friends.jsx
@@ -9,7 +9,7 @@ export default function Friends() {
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(data => setUsers(data))
+            .then(data => setUsers(data));
     }, []);
 
     return (
